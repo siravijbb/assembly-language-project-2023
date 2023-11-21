b76f719545c4f46754c6a996764c7d36641d9aff
Siravijbb <siravijbb@gmail.com>
2023-11-21 11:11:13 +0700

    Create s5.s

---
+++ Week1/s5.s
@@ -0,0 +1,11 @@
+	AREA SR5,CODE,READONLY
+	ENTRY
+	mov  R0, #3     ; ax = 5
+	mov  R2, #3    ; cx = 10
+	mul  R1,R2,R0        ; ax = ax * cx   ; actually dx:ax = ax * cx
+	MOV  R3, #2
+	SUB R5,R0,R3
+	ADD R7,R5,R1
+	
+LOOP B LOOP
+	END
